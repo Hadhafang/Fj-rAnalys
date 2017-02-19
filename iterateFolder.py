a7aadb19147b3a34d926842790955dd44c384e9f
MikaelRyd <mikaelrydstrom@hotmail.com>
2017-02-19 12:22:44 +0100

    knep&knåp

--- iterateFolder.py
+++ iterateFolder.py
@@ -1,21 +1,24 @@
 
 import os
-import extractMTD
+from extractMTD import extractMTD
 
 '''
 @author: manand83, hadhafang
 '''
+
 class iterateFolder:
     def __init__(self, startDir):
         self.dir = startDir
         self.metadata =[] #a list that will save the meta data components
+
+
+    def test(self):
         os.chdir(self.dir)
-        
         for root, dirs, files in os.walk(self.dir):
             for name in files:
                 if name.endswith('.xml'):
                     if "MTD" in name:
-                        self.metadata.append.extractMTD #it should only append witth the outhermost mtd
+                        self.metadata.append(extractMTD()) #it should only append witth the outhermost mtd
                         self.metadata[-1].TimeCloud(root,name) #For the outermost mtd
                         self.metadata[-1].SunAngle(root,name) #This will always be wrong now
                         # print extractClouds(root,name)
